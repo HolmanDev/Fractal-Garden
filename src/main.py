4d6cf4f915371d0c8f32cbeb63d197ad9356773b
HolmanDev <18399006+HolmanDev@users.noreply.github.com>
2022-09-29 14:16:16 +0200

    add more fractals, including my fern

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
-from math import sin
+import random
+from math import cos, sin, sqrt, pi
 
 def main():
     print("Fractal Garden")
@@ -13,7 +14,6 @@ def main():
 
     background = pygame.Surface(screen.get_size())
     background = background.convert() # Converts to single pixel format, speeding up rendering times
-
     clock = pygame.time.Clock()
     fps = 16
     while 1:
@@ -22,11 +22,53 @@ def main():
                 return
         clock.tick(1000.0/fps)
         background.fill((0, 0, 0))
+        #pixels = fractal(1, int(time/100), origin)
+        #pixels = fern(origin, growth)
+        #pixels = myfern(0, 10, pi / 2, origin)
         time = pygame.time.get_ticks()
-        pixels = fractal(1, int(time/100), (int(width/2), int(height/2)))
+        growth = 1 - 1 / ( 1 + time / 1000)
+        origin = (550, height-100)
+        pixels = myfern(0, 4, 0, time / 1000, 0.02, origin)
         draw_pixels(background, pixels)
         screen.blit(background, (0,0)) # Display the background, starting at (0,0) and going (+,+)
         pygame.display.flip() # Update display
+
+def myfern(order, end, orientation, sway, swayScale, origin):
+    newPixels = []
+    ox = origin[0]
+    oy = origin[1]
+    scale = 20 / (3**order)
+    pos = lambda x, y: (
+        round(ox + cos(orientation) * x - sin(orientation) * y), 
+        round(oy - sin(orientation) * x - cos(orientation) * y)
+    )
+    for i in range(0, round(5 * scale)):
+        newPixels.append(pos(0, i))
+    if order < end:
+        return(newPixels + 
+            myfern(order+0.3, end, orientation + pi/24 + sin(sway) * swayScale, sway, swayScale, pos(0, 5 * scale)) +
+            myfern(order+1, end, orientation + pi/2.5 + sin(sway) * swayScale, sway, swayScale, pos(-1, 3 * scale)) +
+            myfern(order+1, end, orientation - pi/4 + sin(sway) * swayScale, sway, swayScale, pos(1, 3 * scale))
+        )
+    else:
+        return newPixels
+
+def fern(origin, growth):
+    pixels = []
+    x = 0
+    y = 0
+    for n in range(11000):
+        r = random.random()
+        if r < 0.01:
+            x, y =  0.00 * x + 0.00 * y,  0.00 * x + 0.16 * y + 0.00
+        elif r < 0.86:
+            x, y =  0.85 * x + 0.04 * y, -0.04 * x + 0.85 * y + 1.60
+        elif r < 0.93:
+            x, y =  0.20 * x - 0.26 * y,  0.23 * x + 0.22 * y + 1.60
+        else:
+            x, y = -0.15 * x + 0.28 * y,  0.26 * x + 0.24 * y + 0.44
+        pixels.append((x * 65 * growth**2 + origin[0], growth * y * -37 + origin[1]))
+    return pixels
 
 # Spiral fractal
 def fractal(order, end, origin):
